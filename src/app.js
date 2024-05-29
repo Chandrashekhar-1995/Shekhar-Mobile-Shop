5e1429d447748758c83368ee72e5db4c5e848e7d
Chandra Shekhar <jmbd.group@gmail.com>
2024-05-29 13:55:03 +0530

    app secure setup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import { LISTEN_DATA_LIMIT } from "./constants.js";
 import cookieParser from "cookie-parser";
 
 const app = express()
@@ -8,5 +9,17 @@ app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
+
+
+//Security / best practices
+app.use(express.json({ limit: LISTEN_DATA_LIMIT }))
+// data from url
+app.use(express.urlencoded({ extended: true, limit: LISTEN_DATA_LIMIT }))
+
+app.use(express.static("public"))
+
+// save secure cookies in browser
+app.use(cookieParser())
+    
 
 export {app}
